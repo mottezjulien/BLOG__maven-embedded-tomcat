e9d587650f4b492845616911c3457702fcb93dc8
Julien <mottez.julien@gmail.com>
2017-07-05 08:49:45 +0200

    [maven-embedded-tomcat] - add plugin tomcat in deploy life cycle - add java8 check

--- src/main/java/fr/jmottez/lessons/HelloWorldServlet.java
+++ src/main/java/fr/jmottez/lessons/HelloWorldServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @WebServlet("/hello")
@@ -13,6 +15,18 @@ public class HelloWorldServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.getOutputStream().println("Hello World !!");
+        List<String> list = new ArrayList<>();
+        list.add("value1");
+        list.add("value2");
+        list.stream().forEach(value -> {
+            try {
+                res.getOutputStream().println(value);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
     }
 
 }
